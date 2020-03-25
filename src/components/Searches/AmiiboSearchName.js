4b8fa9a5410bd1564a7b45466aa1605ab177f921
William Chrapcynski <wchrapcynski@gmail.com>
2020-03-25 15:56:36 -0400

    Search name component has been refactored with hooks

--- src/components/Searches/AmiiboSearchName.js
+++ src/components/Searches/AmiiboSearchName.js
@@ -1,60 +1,48 @@
-import React, { Component } from "react";
-// import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import "../Amiibo.css";
 import "./AmiiboSearchName.css";
 
-class AmiiboSearchName extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { searchArray: [], searchName: "", isLoading: false };
-  }
+function AmiiboSearchName(props) {
+  const [searchName, setSearchName] = useState("");
 
-  searchByName = event => {
+  const searchByName = event => {
     event.preventDefault();
-    this.setState({ isLoading: true });
-    fetch(this.props.apiURL + "name/" + this.state.searchName, {})
+    fetch(props.apiURL + "name/" + searchName)
       .then(res => res.json())
       .then(res => {
         console.log("Got it!");
-        this.setState({
-          searchArray: res,
-          isLoading: false
-        });
-        this.props.setSearchArray(res);
+        props.setSearchArray(res);
       })
       .catch(err => {
         console.log("We've got a problem, sir.", err);
       });
   };
 
-  setName = event => {
-    this.setState({ searchName: event.target.value });
+  const setNameHandler = event => {
+    setSearchName(event.target.value);
   };
 
-  render() {
-    return (
-      <div>
-        <div className="amiibo-search-name form-group">
-          <form className="form-inline">
-            <input
-              type="text"
-              placeholder="By Name"
-              onChange={this.setName}
-              className="form-control input-search"
-            />
-            <div className="space-five"></div>
-            <button
-              className="btn btn-primary"
-              type="submit"
-              onClick={this.searchByName}
-            >
-              Search
-            </button>
-          </form>
-        </div>
+  return (
+    <div>
+      <div className="amiibo-search-name form-group">
+        <form className="form-inline">
+          <input
+            type="text"
+            placeholder="By Name"
+            onChange={setNameHandler}
+            className="form-control input-search"
+          />
+          <div className="space-five"></div>
+          <button
+            className="btn btn-primary"
+            type="submit"
+            onClick={searchByName}>
+            Search
+          </button>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default AmiiboSearchName;
